d9420883d9277972162d59dd4b85709f5133fbbe
Daniel Williams <danielwilliams@Daniels-MacBook-Pro-2.local>
2021-09-13 22:55:20 -0400

    Complete Part 2 of Flix app

---
+++ Flix/Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Daniel Williams on 9/13/21.
+//  Copyright © 2021 Daniel Williams. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flix/Flix/Flix/MoviesViewController.swift
+++ Flix/Flix/Flix/MoviesViewController.swift
@@ -64,19 +64,24 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         
-        cell.posterView.af_setImage(withURL: posterUrl!)
+        cell.imageView!.af_setImage(withURL: posterUrl!)
         
         return cell
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
 }
